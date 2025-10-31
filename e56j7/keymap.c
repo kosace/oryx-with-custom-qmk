280ab44dfdf0c15360921f6672c2b7fe20fb4701
Robin Kosanke <robin.kosanke@linearlead.de>
2025-10-31 09:56:23 -0300

    test

--- e56j7/keymap.c
+++ e56j7/keymap.c
@@ -127,7 +127,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case COPY_MACRO:
       if (record->event.pressed) {
 
-      SEND_STRING(SS_LGUI(SS_TAP(X_C)));
+      // SEND_STRING(SS_LGUI(SS_TAP(X_C)));
+      SEND_STRING("test");
       }
       return false;
 
