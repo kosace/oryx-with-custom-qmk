db59ed94af9e5ee0d8987203671feb033223c15d
Robin Kosanke <robin.kosanke@linearlead.de>
2025-10-31 01:38:01 -0300

    test

--- e56j7/keymap.c
+++ e56j7/keymap.c
@@ -189,7 +189,7 @@ const key_override_t dot_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_DOT, 	L
 const key_override_t comma_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_COMMA, 	KC_GRV);
 const key_override_t semicolon_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_SCLN, 	LSFT(KC_DOT));
 const key_override_t quot_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_QUOT, 	LSFT(KC_2));
-const key_override_t backslash_key_override = ko_make_basic(MOD_MASK_SHIFT, DE_BSLS, 	RALT(KC_NUBS));
+const key_override_t backslash_key_override = ko_make_basic(MOD_MASK_SHIFT, DUAL_FUNC_0, 	RALT(KC_NUBS));
 
 // This globally defines all key overrides to be used
 const key_override_t *key_overrides[] = {
